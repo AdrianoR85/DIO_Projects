c0a28ebbb712ce9114f2d4b120cec92073d48e07
AdrianoR85 <adrianorosa085@gmail.com>
2024-06-12 22:29:15 -0300

    Finished Challenge

--- Python/Challenge_02/bank_system_v2.py
+++ Python/Challenge_02/bank_system_v2.py
@@ -1,23 +1,21 @@
 LIMIT = 500
 LIMIT_WITHDRAW = 3
 AGENCY = '0001'
-users = [{'name': 'Lara', 'cpf': 123456}]
-accounts = [{'account_number': 101010, 'balance': 0}]
+users = []
+accounts = []
 account_number = 000000
   
-def filter_cpf(value, database):
+def filter_cpf(type, value, database):
   field_type = ''
   for row in database:
-    if 'cpf' in row:
-      field_type = 'cpf'
-    else:
-      field_type = 'account_number'
-       
+    if type in row:
+      field_type = type
+  
   filtered_data = [data for data in database if data[field_type] == value]
   return filtered_data[0] if filtered_data else None
 
 def create_address():
-  labels = ['public_area', 'number', 'neighborhood', 'city', 'state']
+  labels = ['number', 'neighborhood', 'city', 'state']
   address = []
   
   print('\n----------Client Address---------\n')
@@ -27,10 +25,10 @@ def create_address():
   
   return address
 
-def create_user(users):
+def create_user(database=users):
   print('\n----------Personal data---------\n')
   cpf = input('Enter CPF: ')
-  user = filter_cpf(cpf, users)
+  user = filter_cpf("cpf", cpf, database)
   if user:
     print('User already exists')
     return 
@@ -42,11 +40,12 @@ def create_user(users):
     'address': create_address()
   }
 
-  users.append(obj_client)
+  database.append(obj_client)
  
-def create_account(AGENCY, users, accounts):
+def create_account(AGENCY, users, database=accounts):
+  global account_number
   cpf = input('Enter CPF: ')
-  user = filter_cpf(cpf, users)
+  user = filter_cpf("cpf", cpf, users)
   
   if not user:
     print('\nUser does not exists\n')
@@ -62,11 +61,12 @@ def create_account(AGENCY, users, accounts):
     'cpf': cpf
   }
   
-  accounts.append(obj_account)
+  print("\nAccount created successfully")
+  database.append(obj_account)
   
 def deposit_amount(amount, account_number, database=accounts):
-  account = filter_cpf(account_number, database)
-  
+  account = filter_cpf("account_number", account_number, database)
+  print(account)
   if account:
     if amount > 0:
       account['balance'] += amount
@@ -77,7 +77,7 @@ def deposit_amount(amount, account_number, database=accounts):
     print("Account does not exists")
     
 def withdraw_amount(amount, account_number, database=accounts):
-  account = filter_cpf(account_number, database )
+  account = filter_cpf("account_number", account_number, database )
   if account:
     if amount > account['balance']:
       print("Insufficient balance")
@@ -92,10 +92,19 @@ def withdraw_amount(amount, account_number, database=accounts):
       account['daily_withdraw'] += 1
       print(f"You have withdrawn {amount:.2f} and your balance is {account['balance']:.2f}")
 
-def statement_amount():
-  print("-------Statement-------")
-  print(f"Your balance is ${balance:.2f}")
-
+def statement_amount(value, database1=users, database2=accounts):
+  user = filter_cpf("cpf", value, database1)
+  account = filter_cpf("cpf", value, database2)
+  
+  if user and account:
+    print("-------Statement-------")
+    print(f"\nName: {user['name']}")
+    print(f"Account: {account['account_number']}")
+    print(f"Balance: {account['balance']:.2f}")
+    print(f"\n=======================\n")
+    return
+  
+  print("User or account does not exists")
 
 def menu():
   title = "Menu"
@@ -110,25 +119,30 @@ def menu():
   6. Exit  
   """)
   
+def main():
+  while True:
+    menu()
+    option = input("> ")
+    if option == "1":
+      account_number = int(input("Enter account number: "))
+      amount = int(input("Enter amount to deposit: "))
+      deposit_amount(amount, account_number)
+    elif option == "2":
+      account_number = int(input("Enter account number: "))
+      amount = int(input("Enter amount to withdraw: "))
+      withdraw_amount(amount, account_number)
+    elif option == "3":
+      cpf = input('Enter CPF: ')
+      statement_amount(cpf)
+    elif option == "4":
+      create_user()
+    elif option == "5":
+      create_account(AGENCY, users, accounts)
+    elif option == "6":
+      break
+    else:
+      print("Invalid option")
+    print("-" * 24)
 
-# while True:
-#   menu()
-#   option = input("> ")
-#   print("-" * 24)
-#   if option == "1":
-#     amount = int(input("Enter amount to deposit: "))
-#     deposit_amount(amount)
-#   elif option == "2":
-#     amount = int(input("Enter amount to withdraw: "))
-#     withdraw_amount(amount)
-#   elif option == "3":
-#     statement_amount()
-#   elif option == "4":
-#     create_user(users)
-#   elif option == "5":
-#     create_account(AGENCY, users, accounts)
-#   elif option == "6":
-#     break
-#   else:
-#     print("Invalid option")
-#   print("-" * 24)
+if __name__ == "__main__":
+  main()
